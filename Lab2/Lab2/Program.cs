ea8807f9d9e586aa59fffc594725f7e84204fe36
egorova <141181932@mail.ru>
2015-12-12 21:57:02 +0500

    Lab2 finished

--- Lab2/Lab2/Program.cs
+++ Lab2/Lab2/Program.cs
@@ -10,6 +10,63 @@ namespace Lab2
     {
         static void Main(string[] args)
         {
+            //Вывести на экран полученные данные
+            Console.Write("Полученные данные: ");
+            Console.WriteLine(args[0]);
+
+            //Запросить у пользователя число для поиска
+            Console.Write("Введите число для поиска: ");
+            int a = Int32.Parse(Console.ReadLine());
+
+            //Разбить строку на массив строк(тип данных String[])
+            String[] string_arr;
+            string_arr = args[0].Split('*');
+
+            //Создать массив чисел(тип данных Int32[])
+            int length = string_arr.Length;
+            Int32[] int_arr;
+            int_arr = new int[length];
+
+            //Используя цикл for преобразовать массив строк в массив чисел
+            for (int i = 0; i < length - 1; i++)
+            {
+                int_arr[i] = Int32.Parse(string_arr[i]);
+            }
+
+            //Бинарный поиск
+            int position = -1;
+            int min_index = 0;
+            int max_index = length;
+            while (min_index != max_index)
+            {
+                position = (min_index + max_index) / 2;
+                if (int_arr[position] == a)
+                {
+                    min_index = max_index;
+                }
+                else
+                {
+                    if (int_arr[position] < a)
+                    {
+                        min_index = position + 1;
+                    }
+                    else
+                    {
+                        max_index = position;
+                    }
+                    position = -1;
+                }
+            }
+            Console.Write("Индекс указанного элемента в массиве: ");
+            if (position != -1)
+            {
+                Console.WriteLine(position);
+            }
+            else
+            {
+                Console.WriteLine("не найден");
+            }
+            Console.ReadLine();
         }
     }
 }
