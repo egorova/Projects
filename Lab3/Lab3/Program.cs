99873e77722532b86dfe57822c4a9e2e93471232
egorova <141181932@mail.ru>
2015-12-13 01:58:23 +0500

    Lab3 finished

--- Lab3/Lab3/Program.cs
+++ Lab3/Lab3/Program.cs
@@ -10,6 +10,52 @@ namespace Lab3
     {
         static void Main(string[] args)
         {
+            //Вывести на экран полученные данные
+            Console.Write("Полученные данные: ");
+            Console.WriteLine(args[0]);
+
+            //Разбить строку на массив строк(тип данных String[])
+            String[] string_arr;
+            string_arr = args[0].Split('-');
+
+            //Создать массив чисел(тип данных Int32[])
+            int length = string_arr.Length;
+            Int32[] int_arr;
+            int_arr = new int[length];
+
+            //Используя цикл for преобразовать массив строк в массив чисел
+            for (int i = 0; i < length - 1; i++)
+            {
+                int_arr[i] = Int32.Parse(string_arr[i]);
+            }
+
+            //Создать список (List<int>) чисел(тип данных int[]) для хранения сортированных данных
+            var int_list = new List<int>();
+            for (int i = 0; i < length - 1; i++)
+            {
+                int_list.Insert(i, int_arr[i]);
+            }
+
+            //Далее необходимо реализовать процедуру сортировки для этого необходимо
+            //использовать 2 цикла: один цикл вложен в другой. Для вставки чисел в 
+            //список использовать метод List<T>.Insert(…)
+            for (int i = 0; i < length -1; i++)
+            {
+                int temp = int_arr[i];
+                int j = i - 1;
+                while (j >= 0 && int_arr[j] > temp)
+                {
+                    int_arr[j + 1] = int_arr[j];
+                    j--;
+                }
+                int_arr[j + 1] = temp;
+            }
+
+            for (int i = 0; i < length - 1; i++)
+            {
+                Console.WriteLine(int_arr[i]);
+            }
+            Console.ReadLine();
         }
     }
 }
